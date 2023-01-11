7710ac38390775857031c385a8a76877b22c2243
Donnell Harris <donnell.harris@koodomobile.com>
2023-01-10 18:43:35 -0800

    testing for day items

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,11 +4,23 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-
+  
   const dayItemClass = classNames('day-list__item', {
     "day-list__item--selected": props.selected,
     "day-list__item--full": (props.spots === 0 ? true: false),
   });
+  
+  // const formatSpots = function(props) {
+  //   if (props.spots === 0) {
+  //      return 'no spots remaining';
+  //   }
+
+  //   if (props.spots === 1) {
+  //     return `${props.spots} spot remaining`;
+  //   }
+
+  //   return `${props.spots} spots remaining`;
+  // }
 
   return (
     <li className={dayItemClass}
@@ -16,7 +28,10 @@ export default function DayListItem(props) {
        selected={props.selected}
        >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}      
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+
     </li>
   );
 }
